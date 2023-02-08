a79377cfd92c49a77761bcc4e6425428ff9a7ba5
Anastasiia-Ni <stasy247@mail.ru>
2023-02-08 07:49:42 -0800

    Jump Game

---
+++ Leetcode_Medium/Jump_Game.cpp
@@ -0,0 +1,43 @@
+/* you are given a 0-indexed array of integers nums of length n. 
+You are initially positioned at nums[0].
+
+Each element nums[i] represents the maximum length of a forward jump from index i. 
+In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+
+0 <= j <= nums[i] and
+i + j < n
+Return the minimum number of jumps to reach nums[n - 1]. 
+The test cases are generated such that you can reach nums[n - 1].
+
+Example 1:
+Input: nums = [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+Input: nums = [2,3,0,1,4]
+Output: 2
+ 
+Constraints:
+1 <= nums.length <= 104
+0 <= nums[i] <= 1000    */
+
+using namespace std;
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int res = 0, cur_end = 0, max_jump = 0;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            max_jump = max(max_jump, i + nums[i]);
+            if (i == cur_end) {
+                res++;
+                cur_end = max_jump;
+            }
+        }
+        return res;
+    }
+};
+
