2eb497d963695c0354dccb2fcd84f94f73cbe16e
Anastasiia-Ni <stasy247@mail.ru>
2023-07-06 11:23:44 -0700

    Minimum Size Subarray Sum

---
+++ Leetcode_Medium/Minimum_Size_Subarray_Sum.cpp
@@ -0,0 +1,46 @@
+/* Given an array of positive integers nums and a positive integer target,
+return the minimal length of a subarray whose sum is greater than or equal to target.
+If there is no such subarray, return 0 instead.
+
+Example 1:
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+
+Example 2:
+Input: target = 4, nums = [1,4,4]
+Output: 1
+
+Example 3:
+Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+Output: 0
+
+Constraints:
+1 <= target <= 109
+1 <= nums.length <= 105
+1 <= nums[i] <= 104 */
+
+using namespace std;
+#include <vector>
+
+class Solution
+{
+public:
+    int minSubArrayLen(int target, vector<int> &nums)
+    {
+        int ans = INT_MAX;
+        int sum = 0;
+
+        for (int i = 0, j = 0; j < nums.size(); j++)
+        {
+            sum += nums[j];
+            while (sum >= target)
+            {
+                ans = min(ans, j - i + 1);
+                sum -= nums[i];
+                i++;
+            }
+        }
+        return ans == INT_MAX ? 0 : ans;
+    }
+};
