3f0e70d779546ac3d3d8ad63e0402f77a0c23667
Anastasiia Nifantova <anastasiianifantova@Anastasiias-MacBook-Pro.local>
2024-02-10 23:24:22 +0100

    Palindromic Substrings

---
+++ Leetcode_Medium/Palindromic_Substrings.cpp
@@ -0,0 +1,57 @@
+/* Given a string s, return the number of palindromic substrings in it.
+A string is a palindrome when it reads the same backward as forward.
+A substring is a contiguous sequence of characters within the string.
+
+Example 1:
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+
+Example 2:
+Input: s = "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ 
+Constraints:
+1 <= s.length <= 1000
+s consists of lowercase English letters. */
+
+using namespace std;
+#include <vector>
+#include <string>
+
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.size();
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        int count = 0;
+
+        // Single chars
+        for (int i = 0; i < n; ++i) {
+            dp[i][i] = true;
+            ++count;
+        }
+
+        // Substrings size 2 chars
+        for (int i = 0; i < n - 1; ++i) {
+            if (s[i] == s[i + 1]) {
+                dp[i][i + 1] = true;
+                ++count;
+            }
+        }
+
+        // Substrings size 3 and more chars
+        for (int len = 3; len <= n; ++len) {
+            for (int i = 0; i <= n - len; ++i) {
+                int j = i + len - 1;
+                if (s[i] == s[j] && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    ++count;
+                }
+            }
+        }
+
+        return count;
+    }
+};
